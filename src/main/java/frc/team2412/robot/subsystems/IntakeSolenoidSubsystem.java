122a8526279ef9b571ece68b85494bdffd21ea08
Ion <Ion@Ion-PC.Home>
2021-02-01 16:55:17 -0800

    Intake subsystem: Created intake solenoid subsystem

---
+++ src/main/java/frc/team2412/robot/subsystems/IntakeSolenoidSubsystem.java
@@ -0,0 +1,26 @@
+package frc.team2412.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team2412.robot.subsystems.constants.IntakeConstants;
+
+public class IntakeSolenoidSubsystem extends SubsystemBase {
+	private final Solenoid intakeSolenoid;
+
+	public IntakeSolenoidSubsystem(Solenoid intakeSolenoid) {
+		this.intakeSolenoid = intakeSolenoid;
+	}
+
+	public void extend() {
+		setSolenoid(IntakeConstants.INTAKE_EXTEND);
+	}
+
+	public void retract() {
+		setSolenoid(IntakeConstants.INTAKE_RETRACT);
+	}
+
+	public void setSolenoid(boolean value) {
+		intakeSolenoid.set(value);
+	}
+
+}
---
+++ src/main/java/frc/team2412/robot/subsystems/constants/IntakeConstants.java
@@ -0,0 +1,6 @@
+package frc.team2412.robot.subsystems.constants;
+
+public class IntakeConstants {
+	public final static boolean INTAKE_EXTEND = true;
+	public final static boolean INTAKE_RETRACT = false;
+}
